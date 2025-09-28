a5c6251b0ee1d207e8cbb8310537e060918f87e7
pritesh <priteshdube@Priteshs-MacBook-Pro.local>
2025-09-28 16:37:29 -0500

    header created

---
+++ lib/Navitems.ts
@@ -0,0 +1,5 @@
+export const navItems:{href:string, title:string}[] = [
+    {href: "/", title: "Dashboard"},
+    {href: "/watchlist", title: "Watchlist"},
+    {href: "/search", title: "Search"},
+]
